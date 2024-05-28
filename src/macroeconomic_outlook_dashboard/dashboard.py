55d05ef7019a6afe6c99790e14a51f728e1c8b56
Baran Açıkel <wbaranacikelw@gmail.com>
2024-05-28 14:32:41 +0200

    Add more countries to dashboard

--- src/macroeconomic_outlook_dashboard/dashboard.py
+++ src/macroeconomic_outlook_dashboard/dashboard.py
@@ -3,13 +3,23 @@ import plotly.graph_objects as go
 import streamlit as st
 from pandas import DataFrame
 
-series_list = [
+high_level_series_list = [
     "OECD/KEI/NAEXKP01.DEU.GP.Q",
     "OECD/KEI/NAEXKP01.FRA.GP.Q",
     "OECD/KEI/NAEXKP01.GBR.GP.Q",
     "OECD/KEI/NAEXKP01.JPN.GP.Q",
     "OECD/KEI/NAEXKP01.ITA.GP.Q",
     "OECD/KEI/NAEXKP01.USA.GP.Q",
+]
+
+second_level_series_list = [
+    "OECD/KEI/NAEXKP01.CHN.GP.Q",
+    # originally China data comes from series "OECD/DP_LIVE/CHN.QGDP.TOT.PC_CHGPP.Q" which does not go along with the others
+    "OECD/KEI/NAEXKP01.BRA.GP.Q",
+    "OECD/KEI/NAEXKP01.MEX.GP.Q",
+    "OECD/KEI/NAEXKP01.ZAF.GP.Q",
+    "OECD/KEI/NAEXKP01.IDN.GP.Q",
+    "OECD/KEI/NAEXKP01.IND.GP.Q",
 ]
 
 
@@ -101,13 +111,9 @@ def draw_multiple_series_trace(df: DataFrame, series_list: list[str], base_perio
     return fig
 
 
-st.set_page_config(layout="wide")
-df_raw = get_series_data(series_list)
-df = pivot_and_slice_dataframe(df_raw, "2021-Q4")
-fig = draw_multiple_series_trace(df_raw, series_list, "2019-Q4")
-
 # ----------output----------
-"# Macroeconomic Outlook - May 2024"
+st.set_page_config(layout="wide")
+"# Macroeconomic Outlook"
 
 "This is a work in progress."
 
@@ -123,6 +129,26 @@ fig = draw_multiple_series_trace(df_raw, series_list, "2019-Q4")
 
 
 "### Quarterly GDP growth rate (% change over the previous period)"
-st.dataframe(df)
 
-fig
+high_level_df_raw = get_series_data(high_level_series_list)
+high_level_df = pivot_and_slice_dataframe(high_level_df_raw, "2021-Q4")
+high_level_df
+
+high_level_fig = draw_multiple_series_trace(
+    high_level_df_raw, high_level_series_list, "2019-Q4"
+)
+high_level_fig
+
+
+"### Growth remains at high levels in emerging Asian countries"
+
+"India, Indonesia and China have overperformed the other developing countries in 2023. South Africa has had a particularly low growth. The OECD have lifted their 2024 growth projections for Brazil and Mexico, but they have revised downwards South-African growth."
+
+second_level_df_raw = get_series_data(second_level_series_list)
+second_level_df = pivot_and_slice_dataframe(second_level_df_raw, "2022-Q1")
+second_level_df
+
+second_level_fig = draw_multiple_series_trace(
+    second_level_df_raw, second_level_series_list, "2019-Q4"
+)
+second_level_fig
